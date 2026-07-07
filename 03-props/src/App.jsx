7d7c4c0d7ad436fc74d399f6295cf1b76471cede
Minor-Project <sonugenius944@gmail.com>
2026-07-07 15:26:44 +0530

    ushing css in index.css and changing sominthing value

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -12,7 +12,7 @@ const App = () =>{
   <Card user="sonu" age={28} />
       <Card user="Avantika" age={28} img='https://plus.unsplash.com/premium_photo-1783095264167-9466440f4a51?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
       <Card user="srshsj" age={28} img= 'https://plus.unsplash.com/premium_photo-1783095264167-9466440f4a51?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
-  <Card user="randi" age={28} />
+  <Card user="rani" age={28} />
    
 
 
